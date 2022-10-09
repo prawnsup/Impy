35f27f143a13675e4a7587e85e1ab830700e9aff
pranshudhungana <pranshudhungana31@gmail.com>
2022-10-09 12:30:13 +0100

    Formatted help section. Changed setup.cfg to get rid of random url.

--- my_cli/cli_t.py
+++ my_cli/cli_t.py
@@ -8,18 +8,43 @@ from .helper import converter
 
 @click.group()
 def impy():
+    '''
+        General usage guide.
+
+        Text files should contain one image/file per line
+        Destination and path can be relative or absolute paths.
+    '''
     pass
 
 
 @click.command('rescale', short_help="rescales images")
 @click.option('--path', '-p', default="", type=click.Path(exists=False),
               help='The area you want this cli to work in, default is current directory')
-@click.option('--rescale', '-re', default=1.0,
+@click.option('--rescale', '-re', default=0,
               help="This is equal to the multiplier you want to rescale your images by.")
 @click.argument('image', default='all', type=click.Path(exists=False))
 @click.option('--height', '-h', default=0.0, help="New height(pixels) of images specified")
 @click.option('--width', '-w', default=0.0, help="New width(pixels) of images specified")
 def rescaler(path, rescale, image, height, width):
+    """
+    \b
+          EXAMPLES
+
+          impy rescale -p /heart -w 520 images.txt\n
+          impy rescale -p /heart -re 2.0 twoX.png\n
+
+
+           IMAGE is the name of image or it's the text file containing names of images to be merged.Each
+           image has to be within path specified.
+
+           If only width or height is specified , aspect ratio is mantained and new height or width is generated.
+
+           If images argument is not specified then every image within the directory will be resized.
+
+            """
+    if height == 0 and width == 0 and rescale == 0:
+        print("[Error] No height/width or rescale argument detected")
+        return
     if image != 'all':
         images_list = converter(image)
     else:
@@ -27,8 +52,8 @@ def rescaler(path, rescale, image, height, width):
             if rescale != 1.0:
                 print(f"[Error] Can't rescale while assigning new width and height.")
                 return
-
         images_list = [image]  # in this case images list = ['all'] or a singular image name ['name']
+
     im = ImageHandler(os.getcwd())
     im.rescale(path, rescale, images_list, height, width)
 
@@ -43,11 +68,17 @@ def rescaler(path, rescale, image, height, width):
 @click.option('--destination', '-d', default=os.getcwd(), type=click.Path(exists=True),
               help="Destination address for merged files.Default is Current directory", required=False)
 def merge_pdf(path, pdfs, add, name, destination):
-    """merge_pdf -p /heart images.txt.
-       merge_pdf -p /heart -n output
+    """
+    \b
+       EXAMPLES
+
+       merge_pdf -p /heart images.txt.\n
+       merge_pdf -p /heart -n output\n
 
        PDFS is the name of original pdf or it's the text file containing names of pdfs to be merged.Each
        pdf has to be within path specified.
+
+       The pages are added after the first image argument.
 
        If pdfs argument is not specified then every PDF within the directory will be merged into a file.
 
@@ -81,18 +112,23 @@ def merge_pdf(path, pdfs, add, name, destination):
 @click.option('--path', '-p', default="", type=click.Path(exists=False),
               help='The area you want to work in, default is current directory')
 @click.argument('image', default='all', type=click.Path(exists=False))  # image can be input as a single image or file
-@click.option('--name', '-n', default='.',
-              help="Name of converted . Default is same name")
-@click.option('--extension', '-ex', help="Extension to be converted to", required=True)
+@click.option('--name', '-n', default='.', help="Name of converted file . Default is same name")
+@click.option('--extension', '-ex', help="Extension to be converted to ", required=True)
 @click.option('--destination', '-d', default="/converted", type=click.Path(exists=False),
-              help="Destination address for converted files")
+              help="Destination address for converted files. Default is new directory called converted.")
 def convert(path, image, name, extension, destination):
-    """ convert -f -p /heart -ex .jpg images.txt
-        convert -p /heart -ex .jpg  -d /jpgs 3.png
+    """ \b
+        EXAMPLES:
 
-       IMAGE is the name of the image .
-       If the --file parameter is specified then its a text file containing names of images.
-            """
+        convert -p /heart -ex .jpg images.txt\n
+        convert -p /heart -ex .jpg  -d jpgs 3.png.\n
+
+        IMAGE is the name of the image/text file containing images.
+
+        Extension is required.
+
+        Cannot name multiple files.
+    """
     if image != 'all':
         # check if file containing imagelist exists
         images_list = converter(image)
@@ -108,3 +144,6 @@ def convert(path, image, name, extension, destination):
 impy.add_command(rescaler)
 impy.add_command(merge_pdf)
 impy.add_command(convert)
+if __name__=='__main__':
+    impy()
+
